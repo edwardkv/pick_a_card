ec3c0e6c40625167f00f883354c622d6b68c95bf
edwardkv <edwardkv@gmail.com>
2020-10-09 12:08:42 +0400

    Symbols of suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 #encoding utf-8
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"] #«бубны», «Червы», «Трефы», «Пики».
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"] #«бубны», «Червы», «Трефы», «Пики».
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
